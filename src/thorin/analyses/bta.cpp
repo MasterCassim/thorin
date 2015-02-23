d74b4353168d34b535f0bb25cd759ce7ce0c8828
Immanuel Haffner <haffner@cs.uni-saarland.de>
2015-02-23 11:40:22 +0100

    "static" lattice values are now in an anonymous namespace

--- src/thorin/analyses/bta.cpp
+++ src/thorin/analyses/bta.cpp
@@ -7,9 +7,10 @@
 
 namespace thorin {
 
-// static lattice values to use
+namespace {
+LV LV_STATIC  = LV(LV::Static);
 LV LV_DYNAMIC = LV(LV::Dynamic);
-LV LV_STATIC = LV(LV::Static);
+}
 
 LV LV::join(LV other) const {
     // TODO: don't always allocate a new LV here?
